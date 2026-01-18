1aeb8ca8b3f389fb07780d3cf7c6e5f41bc68c64
NOOR UL FAJAR <23-22411-048@se.fjwu.edu.pk>
2026-01-18 18:42:00 +0000

    First

---
+++ install_tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "--- Installing Terraform ---"
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt-get install terraform
+
+echo "--- Installing AWS CLI ---"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -o awscliv2.zip
+sudo ./aws/install
+
+echo "--- Installing Ansible ---"
+pip install ansible
+
+echo "--- Verifying Versions ---"
+terraform --version
+aws --version
+ansible --version
+
+echo "Done! Good luck on the exam."
